eeb05f67ff7f489d94d6fe3bf1dbc39ec8dee2cd
monishkumar7 <monishkumar1154@gmail.com>
2021-12-29 22:02:49 -0500

    add anchor and serum

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, Provider, web3 } from '@project-serum/anchor';
+
+import idl from './idl.json';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ConnectWallet from './components/ConnectWallet';
 import AppList from './components/AppList';
 
+const { SystemProgram, Keypair } = web3;
+
+let baseAccount = Keypair.generate();
+
+const programID = new PublicKey(idl.metadata.address);
+
+const network = clusterApiUrl('devnet');
+
+const opts = {
+  preflightCommitment: 'processed'
+};
+
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
+
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new Provider(
+      connection,
+      window.solana,
+      opts.preflightCommitment
+    );
+    return provider;
+  };
 
   const checkIfWalletIsConnected = async () => {
     try {
